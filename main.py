006ec32a20ef9076e730d44c5b356e62534bf054
Andres Becerra <abecerra.aabg@gmail.com>
2019-05-13 14:35:44 -0500

    extensiones y formularios

--- main.py
+++ main.py
@@ -1,23 +1,72 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'clave_secreta'
 
 colors = ['azul', 'amarillo', 'verde', 'rojo']
+
+
+class contactForm(FlaskForm):
+    name = StringField('Usuario', validators=[DataRequired("Campo requerido")])
+    email = EmailField('Correo electronico', validators=[
+                       DataRequired("Campo requerido")])
+    message = TextAreaField('Asunto')
+    # password = PasswordField('Contrase&ntilde;a', validators=[DataRequired()])
+    submit = SubmitField()
 
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
+    # response.set_cookie('user_ip', user_ip)
     return response
 
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    # user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'listColors': colors
     }
     return render_template('hello.html', **context)
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    contact_form = contactForm()
+    contact_name = session.get('name')
+
+    context = {
+        'form': contact_form,
+        'name': contact_name
+    }
+
+    if contact_form.validate_on_submit():
+        name = contact_form.name.data
+        session['name'] = name
+        flash('Recibimos tu mensaje', 'success')
+        return redirect(url_for('contact'))
+
+    return render_template('contact.html', **context)
+
+
+@app.errorhandler(404)
+@app.errorhandler(500)
+def error_handler(error):
+    return render_template('shared/error.html', error=error)
